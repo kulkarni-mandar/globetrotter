b2fe51c15288c75356995d71d6cffcf464e737f6
Mandar Kulkarni <mandar1308@gmail.com>
2025-03-01 19:12:48 +0530

    moved reload into db

--- server/pkg/controller/internal.go
+++ server/pkg/controller/internal.go
@@ -1,13 +1,9 @@
 package controller
 
 import (
-	"encoding/json"
 	"globetrotter/pkg/config"
 	"globetrotter/pkg/database"
-	"globetrotter/pkg/logging"
-	"globetrotter/pkg/models"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,65 +23,13 @@ func configController(c *gin.Context) {
 }
 
 func refreshDataset(c *gin.Context) {
-	db := database.Get()
-
-	db.Exec("truncate cities;")
-	db.Exec("truncate clues;")
-	db.Exec("truncate facts;")
-
-	type data struct {
-		City    string   `json:"city"`
-		Country string   `json:"country"`
-		Clues   []string `json:"clues"`
-		Facts   []string `json:"facts"`
-	}
-
-	var dataset []data
-
-	fileData, err := os.ReadFile("dataset.json")
+	err := database.ReloadDataset("dataset.json")
 	if err != nil {
-		logging.Error("failed to read dataset.json", "error", err)
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "failed to find dataset",
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to reload dataset",
 		})
-
-		return
 	}
 
-	err = json.Unmarshal(fileData, &dataset)
-	if err != nil {
-		logging.Error("failed to unmarshal dataset.json", "error", err)
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "failed to load dataset",
-		})
+	c.JSON(http.StatusOK, gin.H{"message": "success"})
 
-		return
-	}
-
-	for _, record := range dataset {
-		city := &models.City{
-			City:    record.City,
-			Country: record.Country,
-		}
-
-		db.Create(city)
-
-		for _, clue := range record.Clues {
-			db.Create(&models.Clue{
-				CitiesID: city.ID,
-				Clue:     clue,
-			})
-		}
-
-		for _, fact := range record.Facts {
-			db.Create(&models.Fact{
-				CitiesID: city.ID,
-				Fact:     fact,
-			})
-		}
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "success",
-	})
 }
--- server/pkg/database/database.go
+++ server/pkg/database/database.go
@@ -1,10 +1,12 @@
 package database
 
 import (
+	"encoding/json"
 	"fmt"
 	"globetrotter/pkg/config"
 	"globetrotter/pkg/logging"
 	"globetrotter/pkg/models"
+	"os"
 
 	"gorm.io/gorm"
 )
@@ -40,4 +42,55 @@ func New() (*gorm.DB, error) {
 
 func Get() *gorm.DB {
 	return db
+}
+
+func ReloadDataset(filePath string) error {
+
+	db.Exec("truncate cities;")
+	db.Exec("truncate clues;")
+	db.Exec("truncate facts;")
+
+	type data struct {
+		City    string   `json:"city"`
+		Country string   `json:"country"`
+		Clues   []string `json:"clues"`
+		Facts   []string `json:"facts"`
+	}
+
+	var dataset []data
+
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to read dataset.json: %w", err)
+	}
+
+	err = json.Unmarshal(fileData, &dataset)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal dataset.json: %w", err)
+	}
+
+	for _, record := range dataset {
+		city := &models.City{
+			City:    record.City,
+			Country: record.Country,
+		}
+
+		db.Create(city)
+
+		for _, clue := range record.Clues {
+			db.Create(&models.Clue{
+				CitiesID: city.ID,
+				Clue:     clue,
+			})
+		}
+
+		for _, fact := range record.Facts {
+			db.Create(&models.Fact{
+				CitiesID: city.ID,
+				Fact:     fact,
+			})
+		}
+	}
+
+	return nil
 }
